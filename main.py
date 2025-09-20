ac9bfa3fda2772db1b74ba7a11628242d10c1026
LuminaryNova <shyamdharsan18@gmail.com>
2025-09-20 17:58:53 +0530

    first-actual-commit(lol)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import streamlit as st
+from analyze import analyze_question
+from google import genai
+import dspy
+import streamlit as st  
+
+client = genai.Client(api_key="AIzaSyDvyGaKRxXsjUNT7RugtIlrOeYlGhscdoM")
+
+def gemini(prompt):
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=prompt)
+    return response.text
+
+test_question = "What is 2 + 2?"
+print(analyze_question(test_question, gemini))
